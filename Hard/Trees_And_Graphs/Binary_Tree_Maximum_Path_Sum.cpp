bce04b39993cf774501e849e746f9560af2a976a
samuele-galli <samuele.aggeggio@gmail.com>
2026-02-05 12:58:01 +0100

    Max_Tree of hell

--- Hard/Trees_And_Graphs/Binary_Tree_Maximum_Path_Sum.cpp
+++ Hard/Trees_And_Graphs/Binary_Tree_Maximum_Path_Sum.cpp
@@ -38,11 +38,145 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+using namespace std;
+
+#include <iostream>
+#include <climits>
+
+int result;
 
 class Solution
 {
 public:
+    int find_max(int right_node, int left_node, int temp_result)
+    {
+        int node = temp_result;
+        int right_arm = right_node + node;
+        int left_arm = left_node + node;
+
+        if (left_arm > temp_result)
+            temp_result = left_arm;
+        if (right_arm > temp_result)
+            temp_result = right_arm;
+
+        if (node > result)
+            result = node;
+        if (right_arm > result)
+            result = right_arm;
+        if (left_arm > result)
+            result = left_arm;
+        if (node + left_node + right_node > result)
+            result = node + left_node + right_node;
+
+        return (temp_result);
+    }
+    int max_finded_leaves(TreeNode *root)
+    {
+        int left_node = 0;
+        int right_node = 0;
+        int temp_result = root->val;
+
+        if (root->left)
+            left_node = max_finded_leaves(root->left);
+        if (root->right)
+            right_node = max_finded_leaves(root->right);
+
+        return (find_max(right_node, left_node, temp_result));
+    }
+
     int maxPathSum(TreeNode *root)
     {
+        if (!root)
+            return (0);
+        if (!root->left && !root->right)
+            return (root->val);
+        result = INT_MIN;
+        max_finded_leaves(root);
+        return (result);
     }
 };
+
+void delete_tree(TreeNode *root)
+{
+    if (!root)
+        return;
+    delete_tree(root->left);
+    delete_tree(root->right);
+    delete (root);
+    root = nullptr;
+}
+
+int main()
+{
+    Solution s;
+    TreeNode *head;
+    int result;
+
+    head = new TreeNode(1);
+    head->left = new TreeNode(2);
+    head->right = new TreeNode(3);
+
+    result = s.maxPathSum(head);
+    cout << "\n(result finale = " << result << ")\n";
+
+    delete_tree(head);
+
+    head = new TreeNode(-10);
+    head->left = new TreeNode(9);
+    head->right = new TreeNode(20);
+    head->right->right = new TreeNode(7);
+    head->right->left = new TreeNode(15);
+
+    result = s.maxPathSum(head);
+    cout << "\n(result = " << result << ")\n";
+
+    delete_tree(head);
+
+    head = new TreeNode(1);
+    head->left = new TreeNode(2);
+    head->left->left = new TreeNode(100);
+    head->right = new TreeNode(3);
+
+    result = s.maxPathSum(head);
+    cout << "\n(result = " << result << ")\n";
+
+    delete_tree(head);
+
+    head = new TreeNode(-3);
+
+    result = s.maxPathSum(head);
+    cout << "\n(result = " << result << ")\n";
+
+    delete_tree(head);
+
+    head = new TreeNode(1);
+    head->right = new TreeNode(-3);
+    head->right->left = new TreeNode(-2);
+    head->left = new TreeNode(-2);
+    head->left->right = new TreeNode(3);
+    head->left->left = new TreeNode(1);
+    head->left->left->left = new TreeNode(-1);
+
+    result = s.maxPathSum(head);
+    cout << "\n(result = " << result << ")\n";
+
+    delete_tree(head);
+
+    head = new TreeNode(-2);
+    head->right = new TreeNode(-1);
+
+    result = s.maxPathSum(head);
+    cout << "\n(result = " << result << ")\n";
+
+    delete_tree(head);
+
+    head = new TreeNode(20);
+    head->left = new TreeNode(-3);
+    head->left->right = new TreeNode(2);
+    head->left->left = new TreeNode(2);
+
+    result = s.maxPathSum(head);
+    cout << "\n(result = " << result << ")\n";
+
+    delete_tree(head);
+}
