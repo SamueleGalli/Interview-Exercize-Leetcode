6066bc93a669acecfefbb3f0c7e6f7bd22c2c09f
samuele-galli <samuele.aggeggio@gmail.com>
2026-03-24 16:47:08 +0100

    fixed 2 point of hell finally

--- Hard/Math/Max_Points_on_a_Line.cpp
+++ Hard/Math/Max_Points_on_a_Line.cpp
@@ -28,12 +28,77 @@ using namespace std;
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <limits.h>
+#include <cmath>
+#include <map>
 
 class Solution
 {
+private:
+    int gcd(int x, int y)
+    {
+        int result;
+
+        x = abs(x);
+        y = abs(y);
+
+        if (y == 0)
+            return (x);
+
+        result = gcd(y, x % y);
+
+        return (result);
+    }
+
+    int get_points(vector<vector<int>> &points, map<pair<int, int>, int> &occurences, size_t i)
+    {
+        int result = 0;
+        int x = 0;
+        int y = 0;
+
+        for (size_t j = 0; j < points.size(); j++)
+        {
+            x = points[j][0] - points[i][0];
+            y = points[j][1] - points[i][1];
+            if (i != j)
+            {
+                if (x == 0)
+                    occurences[{x, 1}]++;
+                else if (y == 0)
+                    occurences[{1, y}]++;
+                else
+                {
+                    int divisor = gcd(x, y);
+
+                    x = x / divisor;
+                    y = y / divisor;
+                    if (x < 0)
+                    {
+                        x = -x;
+                        y = -y;
+                    }
+                    occurences[{x, y}]++;
+                }
+            }
+        }
+        for (auto value : occurences)
+            result = max(result, value.second);
+        return (result);
+    }
+
 public:
     int maxPoints(vector<vector<int>> &points)
     {
+        map<pair<int, int>, int> occurences;
+        int total = 0;
+
+        for (size_t i = 0; i < points.size(); i++)
+        {
+            total = max(total, get_points(points, occurences, i) + 1);
+            occurences.clear();
+        }
+        return (total);
     }
 };
 
@@ -43,11 +108,27 @@ int main()
     vector<vector<int>> points;
     int maxP;
 
+    points = {{1, 1}};
+    maxP = s.maxPoints(points);
+    cout << "maxP = " << maxP << endl;
+
+    points = {{1, 1}, {2, 2}};
+    maxP = s.maxPoints(points);
+    cout << "maxP = " << maxP << endl;
+
     points = {{1, 1}, {2, 2}, {3, 3}};
     maxP = s.maxPoints(points);
     cout << "maxP = " << maxP << endl;
 
     points = {{1, 1}, {3, 2}, {5, 3}, {4, 1}, {2, 3}, {1, 4}};
+    maxP = s.maxPoints(points);
+    cout << "maxP = " << maxP << endl;
+
+    points = {{0, 0}, {4, 5}, {7, 8}, {8, 9}, {5, 6}, {3, 4}, {1, 1}};
+    maxP = s.maxPoints(points);
+    cout << "maxP = " << maxP << endl;
+
+    points = {{2, 3}, {3, 3}, {-5, 3}};
     maxP = s.maxPoints(points);
     cout << "maxP = " << maxP << endl;
 }
