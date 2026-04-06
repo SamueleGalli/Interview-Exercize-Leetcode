1736c3fea83f18494f99e3052a8b2c1dabd24632
samuele-galli <samuele.aggeggio@gmail.com>
2026-04-06 16:50:08 +0200

    last hard one

--- Hard/Others/Largest_Rectangle_in_Histogram.cpp
+++ Hard/Others/Largest_Rectangle_in_Histogram.cpp
@@ -1,7 +1,7 @@
 /*
 Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
 
- 
+
 
 Example 1:
 
@@ -15,7 +15,7 @@ Example 2:
 Input: heights = [2,4]
 Output: 4
 
- 
+
 
 Constraints:
 
@@ -27,11 +27,47 @@ using namespace std;
 
 #include <vector>
 #include <iostream>
+#include <queue>
 
-class Solution {
+class Solution
+{
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        
+    int give_max(deque<size_t> &range, const vector<int> &heights,
+                 const size_t i, int result,
+                 int value = 0, int occurences = 0)
+    {
+        while (!range.empty())
+        {
+            if (i < heights.size() && heights[range.back()] < heights[i])
+                break;
+            value = heights[range.back()];
+            range.pop_back();
+            if (range.empty())
+                occurences = i;
+            else
+                occurences = i - range.back() - 1;
+            result = max(result, occurences * value);
+        }
+        return (result);
+    }
+
+    int largestRectangleArea(vector<int> &heights)
+    {
+        deque<size_t> range;
+        int result = 0;
+        size_t i = 0;
+        for (; i < heights.size(); i++)
+        {
+            if (range.empty() || heights[range.back()] < heights[i])
+                range.push_back(i);
+            else
+            {
+                result = give_max(range, heights, i, result);
+                range.push_back(i);
+            }
+        }
+        result = give_max(range, heights, i, result);
+        return (result);
     }
 };
 
@@ -41,7 +77,11 @@ int main()
     Solution s;
     int result;
 
-    heights;
+    heights = {2, 1, 5, 6, 2, 3};
+    result = s.largestRectangleArea(heights);
+    cout << "result = " << result << endl;
+
+    heights = {2, 4};
     result = s.largestRectangleArea(heights);
     cout << "result = " << result << endl;
 }
